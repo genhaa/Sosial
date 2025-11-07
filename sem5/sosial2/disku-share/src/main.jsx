8e201b976174d735b6fa5239ee34c05c5ce5a8df
Genha <184705356+14-115-Grace@users.noreply.github.com>
2025-11-07 23:54:56 +0700

    Fix import and usage of AuthProvider in main.jsx

--- sem5/sosial2/disku-share/src/main.jsx
+++ sem5/sosial2/disku-share/src/main.jsx
@@ -4,13 +4,13 @@ import App from './App.jsx';
 import { ThemeProvider } from 'styled-components';
 import { GlobalStyles } from './styles/GlobalStyles.js';
 import { theme } from './styles/theme.js';
-import { AuthProvider } from './context/AuthContext.jsx'; // <-- IMPORT SATPAM
+import { AuthProvider } from './context/AuthContext.jsx'; 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <AuthProvider> {/* <-- BUNGKUS APP PAKE AUTH */}
+      <AuthProvider>
         <App />
       </AuthProvider>
     </ThemeProvider>
